04a57186432cd8a45f6d8ec2f377db33cb83158f
Groskilled <adam.wybierala@gmail.com>
2017-10-03 15:42:01 +0200

    Next is finding the curvature in meters

--- calibrate.py
+++ calibrate.py
@@ -6,22 +6,22 @@ import matplotlib.image as mpimg
 import pickle
 
 def perspective_transform(img):
-    #src = np.float32([corners[0], corners[nx-1], corners[-1], corners[-nx]])
-    src = np.float32([[200,650], [1100,650], [750,450], [550,450]])
-    #plt.imshow(img)
+    src = np.float32([[550,460], [730,460], [1180,670], [100,670]])
     img_size = (img.shape[1], img.shape[0])
+    #plt.imshow(img)
     #for p in src:
     #    plt.scatter(p[0],p[1])
+    #plt.show()
     offset = 0
     dst = np.float32([[offset, offset], [img_size[0]-offset, offset],[img_size[0]-offset, img_size[1]-offset],[offset, img_size[1]-offset]])
     M = cv2.getPerspectiveTransform(src, dst)
     warped = cv2.warpPerspective(img, M, img_size)
-    plt.imshow(warped)
-    plt.show()
-    
-
+    #plt.imshow(warped)
+    #plt.axvline(x=200)
+    #plt.axvline(x=1150)
     #plt.show()
-
+    return warped
+    
 def get_binary_img(img, thresh=(100,255)):
     #print(img.shape)
     bgr = np.copy(img)
@@ -57,11 +57,9 @@ def get_binary_img(img, thresh=(100,255)):
     # Combine the two binary thresholds
     combined_binary = np.zeros_like(sxbinary)
     combined_binary[(s_binary == 1) | (sxbinary == 1)] = 1
-    
-    # Plotting thresholded images
-    plt.imshow(combined_binary)
-    plt.show()
-
+    #plt.imshow(combined_binary)
+    #plt.show()
+    return combined_binary
 
 # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
 objp = np.zeros((6*9,3), np.float32)
@@ -76,9 +74,108 @@ imgpoints = [] # 2d points in image plane.
 images = glob.glob('test_images/*.jpg')
 
 # Step through the list and search for chessboard corners
-for idx, fname in enumerate(images[:5]):
+for idx, fname in enumerate(images):
     img = mpimg.imread(fname)
-    perspective_transform(img)
+    binary = get_binary_img(img)
+    binary_warped = perspective_transform(binary)
+    #binary_warped = perspective_transform(img)
+    out_img = np.dstack((binary_warped, binary_warped, binary_warped))*255
+
+    histogram = np.sum(binary_warped[binary_warped.shape[0]//2:,:], axis=0)
+    midpoint = np.int(histogram.shape[0]/2)
+    leftx_base = np.argmax(histogram[:midpoint])
+    rightx_base = np.argmax(histogram[midpoint:]) + midpoint
+
+    nwindows = 10
+    # Set height of windows
+    window_height = np.int(binary_warped.shape[0]/nwindows)
+    # Identify the x and y positions of all nonzero pixels in the image
+    nonzero = binary_warped.nonzero()
+    nonzeroy = np.array(nonzero[0])
+    nonzerox = np.array(nonzero[1])
+    # Current positions to be updated for each window
+    leftx_current = leftx_base
+    rightx_current = rightx_base
+    # Set the width of the windows +/- margin
+    margin = 100
+    # Set minimum number of pixels found to recenter window
+    minpix = 50
+    # Create empty lists to receive left and right lane pixel indices
+    left_lane_inds = []
+    right_lane_inds = []
+
+    for window in range(nwindows):
+        # Identify window boundaries in x and y (and right and left)
+        win_y_low = binary_warped.shape[0] - (window+1)*window_height
+        win_y_high = binary_warped.shape[0] - window*window_height
+        win_xleft_low = leftx_current - margin
+        win_xleft_high = leftx_current + margin
+        win_xright_low = rightx_current - margin
+        win_xright_high = rightx_current + margin
+        # Draw the windows on the visualization image
+        cv2.rectangle(out_img,(win_xleft_low,win_y_low),(win_xleft_high,win_y_high),
+        (0,255,0), 2)
+        cv2.rectangle(out_img,(win_xright_low,win_y_low),(win_xright_high,win_y_high),
+        (0,255,0), 2)
+        # Identify the nonzero pixels in x and y within the window
+        good_left_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) &
+        (nonzerox >= win_xleft_low) &  (nonzerox < win_xleft_high)).nonzero()[0]
+        good_right_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) &
+        (nonzerox >= win_xright_low) &  (nonzerox < win_xright_high)).nonzero()[0]
+        # Append these indices to the lists
+        left_lane_inds.append(good_left_inds)
+        right_lane_inds.append(good_right_inds)
+        # If you found > minpix pixels, recenter next window on their mean position
+        if len(good_left_inds) > minpix:
+            leftx_current = np.int(np.mean(nonzerox[good_left_inds]))
+        if len(good_right_inds) > minpix:
+            rightx_current = np.int(np.mean(nonzerox[good_right_inds]))
+    
+    # Concatenate the arrays of indices
+    left_lane_inds = np.concatenate(left_lane_inds)
+    right_lane_inds = np.concatenate(right_lane_inds)
+    
+    # Extract left and right line pixel positions
+    leftx = nonzerox[left_lane_inds]
+    lefty = nonzeroy[left_lane_inds]
+    rightx = nonzerox[right_lane_inds]
+    righty = nonzeroy[right_lane_inds]
+    
+    # Fit a second order polynomial to each
+    left_fit = np.polyfit(lefty, leftx, 2)
+    right_fit = np.polyfit(righty, rightx, 2)
+    
+    # Generate x and y values for plotting
+    ploty = np.linspace(0, binary_warped.shape[0]-1, binary_warped.shape[0] )
+    left_fitx = left_fit[0]*ploty**2 + left_fit[1]*ploty + left_fit[2]
+    right_fitx = right_fit[0]*ploty**2 + right_fit[1]*ploty + right_fit[2]
+    
+    out_img[nonzeroy[left_lane_inds], nonzerox[left_lane_inds]] = [255, 0, 0]
+    out_img[nonzeroy[right_lane_inds], nonzerox[right_lane_inds]] = [0, 0, 255]
+    y_eval = np.max(ploty)
+    left_curverad = ((1 + (2*left_fit[0]*y_eval + left_fit[1])**2)**1.5) / np.absolute(2*left_fit[0])
+    right_curverad = ((1 + (2*right_fit[0]*y_eval + right_fit[1])**2)**1.5) / np.absolute(2*right_fit[0])
+
+    ym_per_pix = 30/720 # meters per pixel in y dimension
+    xm_per_pix = 3.7/700 # meters per pixel in x dimension
+    
+    # Fit new polynomials to x,y in world space
+    left_fit_cr = np.polyfit(ploty*ym_per_pix, leftx*xm_per_pix, 2)
+    right_fit_cr = np.polyfit(ploty*ym_per_pix, rightx*xm_per_pix, 2)
+    # Calculate the new radii of curvature
+    left_curverad = ((1 + (2*left_fit_cr[0]*y_eval*ym_per_pix + left_fit_cr[1])**2)**1.5) / np.absolute(2*left_fit_cr[0])
+    right_curverad = ((1 + (2*right_fit_cr[0]*y_eval*ym_per_pix + right_fit_cr[1])**2)**1.5) / np.absolute(2*right_fit_cr[0])
+    # Now our radius of curvature is in meters
+    print(left_curverad, 'm', right_curverad, 'm')
+    #plt.imshow(out_img)
+    #plt.plot(left_fitx, ploty, color='yellow')
+    #plt.plot(right_fitx, ploty, color='yellow')
+    #plt.xlim(0, 1280)
+    #plt.ylim(720, 0)
+
+    #plt.imshow(out_img)
+    #plt.plot(histogram)
+    #plt.show()
     #gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
     ## Find the chessboard corners
@@ -92,9 +189,8 @@ for idx, fname in enumerate(images[:5]):
     #    dst = cv2.undistort(img, mtx, dist, None, mtx)
 
     #    # Draw and display the corners
-    #    #cv2.drawChessboardCorners(img, (9,6), corners, ret)
-    #    #cv2.imshow('img', img)
-    #    cv2.imshow('img', dst)
-    #    cv2.waitKey(500)
+    #cv2.drawChessboardCorners(img, (9,6), corners, ret)
+    #cv2.imshow('img', final)
+    #cv2.waitKey(500)
 
 cv2.destroyAllWindows()
